56702d5385e67cef2741867262ca8cf3cdaf74f1
Manmeet2018 <manietdavv@gmail.com>
2019-12-19 18:56:22 +0530

    added_recursion

---
+++ Recursion-Codes_of_the_string.cpp
@@ -0,0 +1,75 @@
+// Created by Manmeet Singh Parmar
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<ld, ld> pd;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+
+#define FOR(i, x, n) for(ll i=x; i<(n); i++)
+#define F0R(i, n)  for(ll i=0; i<(n); i++)
+#define FORd(i, x, n) for(ll i = (n)-1; i >= x; i--)
+#define F0Rd(i,n) for(ll i = (n)-1; i >= 0; i--)
+#define FOR_B_l(i,n) for(ll i = 0; i < (1LL << n); i++)
+#define FOR_B_r(i,n) for(ll i = 0; i < (1LL >> n); i++) // typecasting of 1 in long long
+
+
+#define l_rot_n(a, n) (aLL << n) // type casting of a in long long and lef rotation of a with n;
+
+#define r_rot_n(a, n) (aLL >> n)
+
+#define mod 1000000007
+#define pi 2acos(0.0)
+#define MP make_pair
+#define PB push_back
+#define EB emplace_back // its's faster than push_back
+#define F first
+#define S second
+#define sz(x) (int)(x).size()
+#define what_is(x) cerr << #x << "is" << x << endl;
+#define gcd(a, b) __gcd(num1 , num2)
+
+string res = "";
+
+string reff = " abcdefghijklmnopqrstuvwxyz";
+
+void Recursion_Codes(string str, int idx)
+{
+	if(!str[idx])
+	{
+		cout<<res<<" ";
+		res = "";
+		return;
+	}
+	res += reff[int(str[idx]-'0')];
+	Recursion_Codes(str,idx+1);
+	if(idx<str.size()-1)
+	{
+		int num = ((str[idx]-'0')*10 +(str[idx+1]-'0'));
+		if(num<27)
+		{
+			res += reff[num];
+			Recursion_Codes(str, idx+1);
+		}
+	}
+}
+int main()
+{
+   	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string str; cin>>str;
+    Recursion_Codes(str, 0); 
+    return 0;
+}
---
+++ move_all_x_to_end.cpp
@@ -0,0 +1,66 @@
+// Created by Manmeet Singh Parmar
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<ld, ld> pd;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+
+#define FOR(i, x, n) for(ll i=x; i<(n); i++)
+#define F0R(i, n)  for(ll i=0; i<(n); i++)
+#define FORd(i, x, n) for(ll i = (n)-1; i >= x; i--)
+#define F0Rd(i,n) for(ll i = (n)-1; i >= 0; i--)
+#define FOR_B_l(i,n) for(ll i = 0; i < (1LL << n); i++)
+#define FOR_B_r(i,n) for(ll i = 0; i < (1LL >> n); i++) // typecasting of 1 in long long
+
+
+#define l_rot_n(a, n) (aLL << n) // type casting of a in long long and lef rotation of a with n;
+
+#define r_rot_n(a, n) (aLL >> n)
+
+#define mod 1000000007
+#define pi 2acos(0.0)
+#define MP make_pair
+#define PB push_back
+#define EB emplace_back // its's faster than push_back
+#define F first
+#define S second
+#define sz(x) (int)(x).size()
+#define what_is(x) cerr << #x << "is" << x << endl;
+#define gcd(a, b) __gcd(num1 , num2)
+
+
+
+// test case
+// input -> xxabcxxdxx
+// output-> abcdxxxxxx
+string moveAllXToEnd(string str)
+{
+	if(!str[0])
+		return "";
+	if(str[0]=='x')
+		return moveAllXToEnd(str.substr(1)) + str[0];
+	return str[0] + moveAllXToEnd(str.substr(1));
+}
+
+
+
+int main()
+{
+   	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    string str; cin>>str;
+    cout<<moveAllXToEnd(str)<<endl;
+    return 0;
+}
